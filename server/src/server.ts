45d6e76b6596feac94d14f95bd0ef4e0dfe6bd2c
alicebranq <branq.alice@gmail.com>
2022-11-03 14:55:35 -0300

    add prisma/client&  pools count

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,19 @@
 import Fastify from "fastify";
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient({
+    log: ['query'],
+})
 
 async function bootstrap() {
-    // criando o servidor
     const fastify = Fastify({
         logger: true,
     })
 
-    // se http://localhost:3333/pools/count, então:
-    fastify.get('/pools/count', () => {
-        return { count: 0 }
+    fastify.get('/pools/count', async () => {
+        const count = await prisma.pool.count()
+
+        return { count }
     }),
 
     await fastify.listen({port:3333})
